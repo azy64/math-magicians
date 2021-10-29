344090831ab91ea3cff8197105059a077519c411
azy64 <allysaidi64@gmail.com>
2021-10-29 11:36:13 +0200

    Add calculate and operate file

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,19 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../css/Button.css';
 
-const Button = ({ width, text, color }) => {
-  const styles = { backgroundColor: `${color}`, width: `${width}` };
-  return (
-    <div style={styles} className="button">
-      <span className="text">{text}</span>
-    </div>
-  );
-};
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  clickHandler = (e) => {
+    const txt = e.target.firstChild.textContent;
+    const { content } = this.props;
+    content(txt);
+  }
+
+  parseStringOp = (str) => {
+    const op = [];
+    const str1 = [...str];
+    // we get the opration in order and get numbers
+    for (let i = 0; i < str1.length; i += 1) {
+      if (str1[i] === '+') op.push('+');
+      if (str1[i] === '-') op.push('-');
+      if (str1[i] === '÷') op.push('÷');
+      if (str1[i] === 'x') op.push('x');
+      if (str1[i] === '%') op.push('%');
+    }
+
+    const reg = /[+\-÷x%]/ig;
+    let tmp = str.replace(reg, ' ');
+    tmp = tmp.split(' ');
+    return tmp;
+  }
+
+  render() {
+    const { width, text, color } = this.props;
+    const styles = { backgroundColor: `${color}`, width: `${width}` };
+    return (
+      <div style={styles} className="button" onClick={this.clickHandler} role="button" tabIndex="0" onKeyPress={this.clickHandler}>
+        <span className="text">{text}</span>
+      </div>
+    );
+  }
+}
 
 Button.propTypes = {
   width: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
+  content: PropTypes.func.isRequired,
 };
 
 export default Button;
