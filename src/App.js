b7a1fdb1decd73dab2df3f81a4e0b430a99eb4bc
azy64 <allysaidi64@gmail.com>
2021-11-03 20:23:01 +0100

    Fix the problem of execution

--- src/App.js
+++ src/App.js
@@ -6,31 +6,23 @@ import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 import Quote from './Components/Quote';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <NavBar />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/calc">
-              <Calculator />
-            </Route>
-            <Route path="/quote">
-              <Quote />
-            </Route>
-          </Switch>
-        </Router>
-
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/calc">
+            <Calculator />
+          </Route>
+          <Route path="/quote">
+            <Quote />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
