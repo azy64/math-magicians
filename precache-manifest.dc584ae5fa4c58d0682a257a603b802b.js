8cad7fb55d07004ccc19e1fa33ef6a8df8cc7a18
azy64 <allysaidi64@gmail.com>
2021-11-05 09:23:16 +0100

    Updates

---
+++ precache-manifest.dc584ae5fa4c58d0682a257a603b802b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2be69dad4822cf75cc7aa534054ea79",
+    "url": "/math-magicians/index.html"
+  },
+  {
+    "revision": "7168afeec6d700cb7e68",
+    "url": "/math-magicians/static/css/main.be615b74.chunk.css"
+  },
+  {
+    "revision": "b93d1f05f13b007525d4",
+    "url": "/math-magicians/static/js/2.f52812fc.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/math-magicians/static/js/2.f52812fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7168afeec6d700cb7e68",
+    "url": "/math-magicians/static/js/main.91a5da1a.chunk.js"
+  },
+  {
+    "revision": "2e06e0e673febfa3e023",
+    "url": "/math-magicians/static/js/runtime-main.a7ffb674.js"
+  }
+]);
